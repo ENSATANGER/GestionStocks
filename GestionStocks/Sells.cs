d315d750ede3b494bd778f1d2febb6c5080ca6f0
Louay <louay.alami.ouahabi@yosobox.com>
2023-04-05 17:57:02 +0000

    sells created

---
+++ GestionStocks/Sells.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace GestionStocks
+{
+    internal class Sells
+    {
+        private static Connection conn = new Connection();
+
+        private static IMongoCollection<Sells> collection = conn.getCollection<Sells>("Sells");
+
+
+
+    }
+}
